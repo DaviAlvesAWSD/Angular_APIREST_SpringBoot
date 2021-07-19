fbe86469fa2b919166faf094888163adf55909df
Davi Alves <75328283+DaviAlvesAWSD@users.noreply.github.com>
2021-07-19 20:18:48 -0300

    atualizacao-19-07-2021-20h17

--- algamoney-api/src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,15 @@
 package com.example.algamoney.api.repository.lancamento;
 
-import java.util.List;
+//import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 	
-	public List<Lancamento> Filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> Filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 
 }
